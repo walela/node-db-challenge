70c24ac4a534a27d6226b86b21ccfcb036fc5412
Austin Walela <austinmasibo@gmail.com>
2020-03-20 14:02:28 +0300

    added resources model and router

--- resources/router.js
+++ resources/router.js
@@ -1,5 +1,28 @@
 const express = require('express')
+const db = require('./model')
 
 const router = express.Router()
 
-module.exports = router;
+router.get('/', (req, res) => {
+  db.getAll()
+    .then(resources => {
+      res.status(200).json(resources)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ message: 'Failed to get resources' })
+    })
+})
+
+router.post('/', (req, res) => {
+  db.insert(req.body)
+    .then(id => {
+      res.status(201).json(`A new resource with an id of ${id} was created`)
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({ message: 'Failed to create resource' })
+    })
+})
+
+module.exports = router
